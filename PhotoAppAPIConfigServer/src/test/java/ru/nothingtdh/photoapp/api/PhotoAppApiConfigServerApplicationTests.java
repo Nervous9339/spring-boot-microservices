7d7f1eead9631ccdfc670a89dd19c2e7c7c6301c
Nikita Zinovev <nervous9339@gmail.com>
2020-01-18 19:47:36 +0300

    Config server added

---
+++ PhotoAppAPIConfigServer/src/test/java/ru/nothingtdh/photoapp/api/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nothingtdh.photoapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
