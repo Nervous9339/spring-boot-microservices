a00bb0a604c7443c00e7f523b6530a1d19ea2f7c
Nikita Zinovev <nervous9339@gmail.com>
2020-01-18 16:53:44 +0300

    Account service added

---
+++ PhotoAppApiAccountManagement/src/test/java/ru/nothingtdh/photoapp/api/account/management/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nothingtdh.photoapp.api.account.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
