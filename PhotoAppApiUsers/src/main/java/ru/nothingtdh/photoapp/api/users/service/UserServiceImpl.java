8012198dfe5ec5cf0e3e419d7e30d15ccacc836a
nikitazinovev <nervous9339@gmail.com>
2020-02-24 20:08:31 +0300

    Section 21 finished

--- PhotoAppApiUsers/src/main/java/ru/nothingtdh/photoapp/api/users/service/UserServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/ru/nothingtdh/photoapp/api/users/service/UserServiceImpl.java
@@ -1,11 +1,9 @@
 package ru.nothingtdh.photoapp.api.users.service;
 
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
